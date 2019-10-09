616adf1a1acf3475217fe811508bb26382aa7b3b
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2019-10-09 10:39:25 +0200

    replace tf.contrib.layers with tf.layers

--- mmd_vae.py
+++ mmd_vae.py
@@ -8,79 +8,124 @@ from tensorflow.examples.tutorials.mnist import input_data
 def lrelu(x, rate=0.1):
     return tf.maximum(tf.minimum(x * rate, 0), x)
 
+
 def conv2d_lrelu(inputs, num_outputs, kernel_size, stride):
-    conv = tf.contrib.layers.convolution2d(inputs, num_outputs, kernel_size, stride,
-                                           weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                           activation_fn=tf.identity)
+    conv = tf.layers.conv2d(
+        inputs,
+        num_outputs,
+        kernel_size,
+        stride,
+        padding="SAME",
+        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+        activation=tf.identity,
+    )
     conv = lrelu(conv)
     return conv
 
+
 def conv2d_t_relu(inputs, num_outputs, kernel_size, stride):
-    conv = tf.contrib.layers.convolution2d_transpose(inputs, num_outputs, kernel_size, stride,
-                                                     weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                                     activation_fn=tf.identity)
+    conv = tf.layers.conv2d_transpose(
+        inputs,
+        num_outputs,
+        kernel_size,
+        strides=stride,
+        padding="SAME",
+        activation=tf.identity,
+        use_bias=True,
+        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+    )
     conv = tf.nn.relu(conv)
     return conv
 
+
 def fc_lrelu(inputs, num_outputs):
-    fc = tf.contrib.layers.fully_connected(inputs, num_outputs,
-                                           weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                           activation_fn=tf.identity)
+    fc = tf.layers.dense(
+        inputs,
+        num_outputs,
+        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+        activation=tf.identity,
+    )
     fc = lrelu(fc)
     return fc
 
+
 def fc_relu(inputs, num_outputs):
-    fc = tf.contrib.layers.fully_connected(inputs, num_outputs,
-                                           weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                           activation_fn=tf.identity)
+    fc = tf.layers.dense(
+        inputs,
+        num_outputs,
+        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+        activation=tf.identity,
+    )
     fc = tf.nn.relu(fc)
     return fc
 
+
 # Encoder and decoder use the DC-GAN architecture
 def encoder(x, z_dim):
-    with tf.variable_scope('encoder'):
+    with tf.variable_scope("encoder"):
         conv1 = conv2d_lrelu(x, 64, 4, 2)
         conv2 = conv2d_lrelu(conv1, 128, 4, 2)
         conv2 = tf.reshape(conv2, [-1, np.prod(conv2.get_shape().as_list()[1:])])
         fc1 = fc_lrelu(conv2, 1024)
-        return tf.contrib.layers.fully_connected(fc1, z_dim, activation_fn=tf.identity)
+        return tf.layers.dense(fc1, z_dim, activation=tf.identity)
+
 
 def decoder(z, reuse=False):
-    with tf.variable_scope('decoder') as vs:
+    with tf.variable_scope("decoder") as vs:
         if reuse:
             vs.reuse_variables()
         fc1 = fc_relu(z, 1024)
-        fc2 = fc_relu(fc1, 7*7*128)
+        fc2 = fc_relu(fc1, 7 * 7 * 128)
         fc2 = tf.reshape(fc2, tf.stack([tf.shape(fc2)[0], 7, 7, 128]))
         conv1 = conv2d_t_relu(fc2, 64, 4, 2)
-        output = tf.contrib.layers.convolution2d_transpose(conv1, 1, 4, 2, activation_fn=tf.sigmoid)
+        output = tf.layers.convolution2d_transpose(
+            conv1, 1, 4, 2, activation=tf.sigmoid, padding="same"
+        )
         return output
+
 
 def compute_kernel(x, y):
     x_size = tf.shape(x)[0]
     y_size = tf.shape(y)[0]
     dim = tf.shape(x)[1]
-    tiled_x = tf.tile(tf.reshape(x, tf.stack([x_size, 1, dim])), tf.stack([1, y_size, 1]))
-    tiled_y = tf.tile(tf.reshape(y, tf.stack([1, y_size, dim])), tf.stack([x_size, 1, 1]))
-    return tf.exp(-tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float32))
+    tiled_x = tf.tile(
+        tf.reshape(x, tf.stack([x_size, 1, dim])), tf.stack([1, y_size, 1])
+    )
+    tiled_y = tf.tile(
+        tf.reshape(y, tf.stack([1, y_size, dim])), tf.stack([x_size, 1, 1])
+    )
+    return tf.exp(
+        -tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float32)
+    )
+
 
 def compute_mmd(x, y):
     x_kernel = compute_kernel(x, x)
     y_kernel = compute_kernel(y, y)
     xy_kernel = compute_kernel(x, y)
-    return tf.reduce_mean(x_kernel) + tf.reduce_mean(y_kernel) - 2 * tf.reduce_mean(xy_kernel)
+    return (
+        tf.reduce_mean(x_kernel)
+        + tf.reduce_mean(y_kernel)
+        - 2 * tf.reduce_mean(xy_kernel)
+    )
+
 
 # Convert a numpy array of shape [batch_size, height, width, 1] into a displayable array
 # of shape [height*sqrt(batch_size, width*sqrt(batch_size))] by tiling the images
 def convert_to_display(samples):
-    cnt, height, width = int(math.floor(math.sqrt(samples.shape[0]))), samples.shape[1], samples.shape[2]
+    cnt, height, width = (
+        int(math.floor(math.sqrt(samples.shape[0]))),
+        samples.shape[1],
+        samples.shape[2],
+    )
     samples = np.transpose(samples, axes=[1, 0, 2, 3])
     samples = np.reshape(samples, [height, cnt, cnt, width])
     samples = np.transpose(samples, axes=[1, 0, 2, 3])
-    samples = np.reshape(samples, [height*cnt, width*cnt])
+    samples = np.reshape(samples, [height * cnt, width * cnt])
     return samples
 
-mnist = input_data.read_data_sets('mnist_data')
+
+mnist = input_data.read_data_sets("mnist_data")
 plt.ion()
 
 # Build the computation graph for training
@@ -112,8 +157,10 @@ for i in range(10000):
     if i % 100 == 0:
         print("Negative log likelihood is %f, mmd loss is %f" % (nll, mmd))
     if i % 500 == 0:
-        samples = sess.run(gen_x, feed_dict={gen_z: np.random.normal(size=(100, z_dim))})
-        plt.imshow(convert_to_display(samples), cmap='Greys_r')
+        samples = sess.run(
+            gen_x, feed_dict={gen_z: np.random.normal(size=(100, z_dim))}
+        )
+        plt.imshow(convert_to_display(samples), cmap="Greys_r")
         plt.show()
         plt.pause(0.001)
 
